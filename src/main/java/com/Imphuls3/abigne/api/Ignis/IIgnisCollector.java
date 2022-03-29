a61b9f7c3d672f5e381134cd7f1d66e90d01b090
1mphuls3 <isaacmorrell13@gmail.com>
2022-03-28 19:27:35 -0700

    Initial commit

--- src/main/java/com/Imphuls3/abigne/api/Ignis/IIgnisCollector.java
+++
@@ -1,9 +0,0 @@
-package com.Imphuls3.abigne.api.Ignis;
-
-public interface IIgnisCollector extends IIgnisReceiver {
-    void onClientDisplayTick();
-
-    float getIgnisYieldMultiplier(IIgnisBurst burst);
-
-    int getMaxIgnis();
-}
